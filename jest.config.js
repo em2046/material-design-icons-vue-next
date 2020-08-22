a3aa9119463e0da81d7b47f958d782d82fa17f5b
em2046 <em2046@qq.com>
2020-08-22 10:49:06 +0800

    test: render

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  preset: 'ts-jest',
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+};
---
+++ tests/icon.test.ts
@@ -0,0 +1,37 @@
+import { mount } from '@vue/test-utils';
+import globby from 'globby';
+import fs from 'fs';
+import path from 'path';
+
+const fsPromises = fs.promises;
+
+describe('icon', () => {
+  jest.setTimeout(60 * 1000);
+
+  test('render', async () => {
+    let src = 'src/icons';
+    let ext = '.tsx';
+    let types = await fsPromises.readdir(src);
+
+    for (const type of types) {
+      let typePath = path.join(src, type);
+      let stats = await fsPromises.stat(typePath);
+
+      if (!stats.isDirectory()) {
+        continue;
+      }
+
+      let files = await globby('*' + ext, {
+        cwd: typePath,
+      });
+
+      for (const file of files) {
+        let relativePath = path.join('..', typePath, file);
+        let imported = await import(relativePath);
+        let component = imported[path.basename(file, ext)];
+        let wrapper = mount(component);
+        expect(wrapper.html()).toMatchSnapshot();
+      }
+    }
+  });
+});
